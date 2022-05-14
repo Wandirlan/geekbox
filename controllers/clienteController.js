318dc4a9b7cffd3dc703cd58d396958719d40784
Wandirlan Rocha <wandirlan.rocha1@outlook.com>
2022-05-14 12:48:27 -0400

    Atualizacao de cod e add de na packge

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,9 +1,32 @@
+// const bcrypt = require('bcrypt');
+// let hash = bcrypt.hashSync("1234", 10);
+// console.log(hash);
+
+const bancoClientes = [
+    {
+        email: "wandirlan.rocha1@outlook.com",
+        senha:"123"
+    }
+]
+
+console.log(bancoClientes[0].email);
+
 const clienteController = {
     login: (req, res) => {
         res.render('login');
     },
     cadastro: (req, res) => {
         res.render('cadastroCliente');
+    },
+    autenticaLogin: (req, res) => {
+        const { email, senha} = req.body;
+        // const clienteEncontrado = bancoClientes.find((obj) => obj.email == email);
+        if( email == bancoClientes[0].email){
+            res.redirect("/");
+        }
+        else{
+            res.redirect("cadastro");
+        }
     }
 };
 
--- routes/cliente.js
+++ routes/cliente.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var clienteController = require('../controllers/clienteController');
+const { body} = require('express-validator');
 
 router.get('/login', clienteController.login);
 router.get('/cadastro', clienteController.cadastro);
-
+router.post('/login', clienteController.autenticaLogin);
 
 module.exports = router;
